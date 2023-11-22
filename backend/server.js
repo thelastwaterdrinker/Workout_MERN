d3693c343ad945b6f60aa3e02ecd0dd3834081e2
thelastwaterdrinker <tugbadoan@gmail.com>
2023-11-22 20:53:35 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+const workoutRoutes = require('./routes/workouts')
+
+//Express app
+const app = express()
+
+//Middleware
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+//Routes
+app.use('/api/workouts', workoutRoutes)
+
+//Connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+app.listen(process.env.PORT, () => {
+    console.log('Connected to MongoDB and listening on PORT', process.env.PORT)
+    })
+})
+.catch((error) => {
+    console.log(error)
+})
+
